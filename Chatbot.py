de56f1499868a3dbfc0c1907a793743df381c296
Rodrigo Almeida <rodrigo.almeida@gmail.com>
2025-05-30 15:08:42 -0300

    Removing menu option

--- Chatbot.py
+++ Chatbot.py
@@ -2,7 +2,6 @@ from openai import OpenAI
 import anthropic
 from dotenv import load_dotenv
 import streamlit as st
-from streamlit_option_menu import option_menu
 import asyncio
 from utils import (
     updateFile, 
